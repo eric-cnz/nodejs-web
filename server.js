e667544a668ab380ce25514cd6bb894e25e78120
Eric-zzz2 <952183847@qq.com>
2025-04-18 22:27:33 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!');
+});
+
+const port = 3000;
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
